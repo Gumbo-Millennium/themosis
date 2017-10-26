697d00a9f44f038189a8f24c15fd97e357cdacaf
Arjen Stens <arjenstens@icloud.com>
2017-10-26 23:33:49 +0200

    home page loading sliders of upcomming events

--- htdocs/content/themes/themosis-theme/resources/controllers/HomeController.php
+++ htdocs/content/themes/themosis-theme/resources/controllers/HomeController.php
@@ -2,12 +2,65 @@
 
 namespace Theme\Controllers;
 
+use Theme\Models\Post;
 use Themosis\Route\BaseController;
 
-class Example extends BaseController
+class HomeController extends BaseController
 {
+
+    /**
+     * @return string
+     */
     public function index()
     {
-        return view('welcome');
+        $data = array();
+
+        /* get slides of upcoming events and pass them to view */
+        $eventSlides = $this->createEventsSlides();
+        $data = array_add($data, 'sliderItems', $eventSlides);
+
+        /* TODO unused variable now, required in template, possibly remove this */
+        $post = new Post();
+        $data = array_add($data, 'post', $post);
+
+
+        /* return view and pass data */
+        return view('home', $data);
+    }
+
+    /**
+     * @return string[][]
+     */
+    public function createEventsSlides(){
+
+        /* get all activities created by WordPress, only future activities get fetched */
+        $events = get_posts([
+            'limit' => 3,
+            'post_status' => 'future',
+            'post_type' => 'activities',
+            'orderBy'    => 'date',
+            'order' => 'ASC',
+            'public' => true
+        ]);
+
+        /* creates slider for every activity by creating */
+        $eventSlides = array();
+        foreach($events as $key => $event){
+            $eventSlide = array();
+
+            /* fill slider item with properties extracted from activity */
+            $eventSlide = array_add($eventSlide, 'caption', $event->post_title);
+            $eventSlide = array_add($eventSlide, 'subCaption', get_the_date('l j F', $event));
+            $eventSlide = array_add($eventSlide, 'featuredImage', get_the_post_thumbnail_url($event->ID, 'full'));
+            $eventSlide = array_add($eventSlide, 'showButton', true);
+            $eventSlide = array_add($eventSlide, 'buttonText', 'Bekijk activiteit');
+            $eventSlide = array_add($eventSlide, 'buttonUrl', get_permalink($event));
+            $eventSlide = array_add($eventSlide, 'alignment', 'right');
+
+            /* add this sliderItem to list of slider items */
+            $eventSlides = array_add($eventSlides, 'slide' . $key, $eventSlide);
+        }
+
+        return $eventSlides;
     }
 }
--- htdocs/content/themes/themosis-theme/resources/views/home.blade.php
+++ htdocs/content/themes/themosis-theme/resources/views/home.blade.php
@@ -40,41 +40,26 @@
 
       <div id="myCarousel" class="carousel slide" data-ride="carousel">
         <ol class="carousel-indicators">
-          <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-          <li data-target="#myCarousel" data-slide-to="1"></li>
-          <li data-target="#myCarousel" data-slide-to="2"></li>
+          {{-- loop sliderItems to add indicators to slider --}}
+          @foreach ($sliderItems as $sliderItem )
+            <li data-target="#myCarousel" data-slide-to="{{ $loop->index }}" @if ($loop->first) class="active" @endif></li>
+          @endforeach
         </ol>
         <div class="carousel-inner">
-          <div class="carousel-item active">
-            <img class="first-slide" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="First slide">
-            <div class="container">
-              <div class="carousel-caption text-left">
-                <h1>Example headline.</h1>
-                <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
-                <p><a class="btn btn-lg btn-primary" href="#" role="button">Sign up today</a></p>
+          @foreach ($sliderItems as $sliderItem )
+            <div class="carousel-item @if ($loop->first) active @endif">
+              <img class="second-slide" src="{{ $sliderItem['featuredImage'] }}" alt="Slide">
+              <div class="container">
+                <div class="carousel-caption text-{{ $sliderItem['alignment'] }}">
+                  <h1>{{ $sliderItem['caption'] }}</h1>
+                  <p>{{ $sliderItem['subCaption'] }}</p>
+                  @if($sliderItem['showButton'] == true)
+                    <p><a class="btn btn-lg btn-primary" href="{{ $sliderItem['buttonUrl'] }}" role="button">{{ $sliderItem['buttonText'] }}</a></p>
+                  @endif
+                </div>
               </div>
             </div>
-          </div>
-          <div class="carousel-item">
-            <img class="second-slide" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Second slide">
-            <div class="container">
-              <div class="carousel-caption">
-                <h1>Another example headline.</h1>
-                <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
-                <p><a class="btn btn-lg btn-primary" href="#" role="button">Learn more</a></p>
-              </div>
-            </div>
-          </div>
-          <div class="carousel-item">
-            <img class="third-slide" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Third slide">
-            <div class="container">
-              <div class="carousel-caption text-right">
-                <h1>One more for good measure.</h1>
-                <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
-                <p><a class="btn btn-lg btn-primary" href="#" role="button">Browse gallery</a></p>
-              </div>
-            </div>
-          </div>
+          @endforeach
         </div>
         <a class="carousel-control-prev" href="#myCarousel" role="button" data-slide="prev">
           <span class="carousel-control-prev-icon" aria-hidden="true"></span>
