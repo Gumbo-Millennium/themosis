23e251f8f727bb769413d0de10ccfa53e0824a19
Roelof Roos <github@roelof.io>
2017-10-26 20:14:00 +0200

    Added style and javascript compiled by Webpack

--- htdocs/content/themes/themosis-theme/resources/routes.php
+++ htdocs/content/themes/themosis-theme/resources/routes.php
@@ -11,13 +11,13 @@
 
 // CSS
 Asset::add('bootstrapcss','https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css');
-Asset::add('carouselbootstrap','css/carousel.css');
-Asset::add('customcss','css/custom.css');
+Asset::add('gumbo-theme-stylesheet','css/gumbo-millennium.css');
 
 // JS
 Asset::add('jquerybootstrap','https://code.jquery.com/jquery-3.2.1.slim.min.js', [], false, true);
 Asset::add('popperjs','https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js', ['jquerybootstrap'], false, true);
 Asset::add('bootstrapjs','https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js', ['jquerybootstrap', 'popperjs'], false, true);
+Asset::add('gumbo-theme-javascript','js/gumbo-millennium.js');
 
 Route::any('front', function($post, $query)
 {
